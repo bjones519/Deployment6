c937b0d7e80fd790674a50c9210583a14fac4fb2
Brittney Jones <87450561+bjones519@users.noreply.github.com>
2021-09-25 19:18:45 -0400

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,4 +1,4 @@
-describe('Heading', () => {
+/*describe('Heading', () => {
     it('has the right title', () => {
         cy.visit('http://184.72.133.230:5000/example-1')
 
@@ -7,4 +7,25 @@ describe('Heading', () => {
             .should("equal", "My Awesome Web Application")
     });
 
+});
+*/
+
+describe('Testing character input',() => { //name of our test
+ 
+    it('check input number',() => { //our test
+
+        cy.visit('http://localhost:5000/example-2')//cypress goes to pg
+        
+        cy.get('span')
+        .invoke('text')
+        .should('equal','15')
+
+        cy.get('input').type('hello')
+
+        cy.get('span')
+        .invoke('text')
+        .should('equal','11')
+
+        
+    });
 });
